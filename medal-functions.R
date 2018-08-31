5355ac960bf24a6ff1b5e4ce010021fccc4a72be
arturolp <arturolp@gmail.com>
2018-08-31 10:53:59 -0700

    test

--- medal-functions.R
+++ medal-functions.R
@@ -400,7 +400,8 @@ medalDistance <- function(pat1, pat2, verbose = FALSE){
     if(dim(sequences[[medication]])[1] == 2){
       
       seq1 = unlist(sequences[[medication]][1,])
-      sequence1 = compactSequence(seq1)
+      #sequence1 = compactSequence(seq1)
+      sequence1 = seq1
       seq2 = unlist(sequences[[medication]][2,])
       #sequence2 = compactSequence(seq2)
       sequence2 = seq2
